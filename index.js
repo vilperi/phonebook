570a9f73c661fa1ed3ffe301dcdef5286e8528af
Vili Rauhala <vili.rauhala@helsinki.fi>
2025-07-14 13:32:19 +0300

    removed cors

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-// delete before deploy
-const cors = require('cors')
-app.use(cors())
-// ---
-
 app.use(express.json())
 app.use(morgan('tiny'))
 
