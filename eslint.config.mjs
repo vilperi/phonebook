47093834c5859dcb355729943591d069703bb9dc
Vili Rauhala <vili.rauhala@helsinki.fi>
2025-07-16 15:24:15 +0300

    linting done

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    },
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      // stylistic rules (drop /js)
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/linebreak-style': ['error', 'unix'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'never'],
+      // standard ESLint rules
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+    },
+  },
+  {
+    ignores: ['dist/**', '.history/**'],
+  },
+]
--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
